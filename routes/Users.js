093849746aa8616290378b8b29156a4d1abe2365
mendozahome <supermerolul@gmail.com>
2023-05-19 00:27:53 -0600

    second

--- routes/Users.js
+++ routes/Users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { Users } = require('../models')
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 const jwt = require('jsonwebtoken');
 const { validateToken } = require('../middlewares/Authmiddleware')
